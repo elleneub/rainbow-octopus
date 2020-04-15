7a4c80e1eb5f85da6cc266fbe16398514edfb68e
solgamd <solgamd@gmail.com>
2020-04-15 14:28:33 -0500

    Added information fields and state values

--- UI/src/components/PostDetails.jsx
+++ UI/src/components/PostDetails.jsx
@@ -1,47 +1,62 @@
 import React from 'react'
 
-
 class PostDetails extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            name: '',
-            title: '',
-            location: '',
-            description: '',
-            notes: '',
-            expiration: '',
-            recurring: false
+            name: 'Josh Burnside',
+            title: 'Need groceries to be picked up',
+            location: 'NE Minneapolis',
+            description:
+                'I need someone to pick up my groceries because I live alone and have a compromised immune system. I live in an apartment in NE Minneapolis so I will need to meet you at the front door to the building.',
+            notes:
+                'I am available in the mornings from 9-10am or evenings from 5-7pm.',
+            expiration: 'Monday May 4, 2020',
+            recurring: false,
         }
     }
     render() {
         return (
-            <div className="card bg-light shadow-sm m-3 border-primary">
-                <div className="card-body post-card ">
-                    <h5 className="card-title font-weight-bold text-left">
-                        Helper: {this.state.name}
+            <div className="card post-details bg-light shadow-sm m-3 border-primary">
+                <div className="card-body text-inline">
+                    <h5 className="card-title font-weight-bold text-center">
+                        Request Details
                     </h5>
+                    <h6 className="card-title font-weight-bold text-left">
+                        Person requesting help: {this.state.name}
+                    </h6>
+                    <h6 className="card-title font-weight-bold text-left">
+                        (Request) {this.state.title}
+                    </h6>
                     <p className="card-subtitle mb-2 text-muted text-left">
                         Location: {this.state.location}
                     </p>
-
-                    <div className="row">
-                        <div className="col text-left">
-                            <p className="card-text">
-                                I need someone to pick up my groceries because I
-                                live alone and have a compromised immune system.
-                            </p>
-                        </div>
-                        <div class="col d-flex text-justify align-item-center">
-                            <button className="btn-primary btn btn-sm d-flex align-self-end">
-                                Learn More
-                            </button>
-                        </div>
-                    </div>
+                    <p className="card-text text-left">
+                        {this.state.description}
+                    </p>
+                    <p className="card-text text-left">
+                        <strong>Notes: </strong>
+                        {this.state.notes}
+                    </p>
+                    <p className="card-text text-left">
+                        <strong>Expiration Date: </strong>
+                        {this.state.expiration}
+                    </p>
+                    <p className="card-text text-left">
+                        <strong>Recurring? </strong>
+                        {this.state.recurring}
+                    </p>
+                    <button className="btn-primary btn btn-sm d-flex align-self-end">
+                        Contact {this.state.name}
+                    </button>
+                    {/* <div className="row">
+                        <div className="col text-left"></div>
+                        <div class="col d-flex text-justify align-item-center"></div>
+                    </div> */}
                 </div>
             </div>
         )
     }
 }
 
-export default PostDetails;
+export default PostDetails
