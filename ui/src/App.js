87f6f278d3f7064a09976cc83a06898ced78cd12
Elle Neubauer <elle@elleneub.com>
2020-04-15 17:18:40 -0500

    Add in spinner, and fake data loadin

--- ui/src/App.js
+++ ui/src/App.js
@@ -10,24 +10,75 @@ import CommunityGuidelinesPage from './pages/CommunityGuidelines.jsx'
 import Navbar from './components/Navbar.jsx'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            userData: {},
+            isLoading: true,
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                userData: {
+                    firstName: 'Stevani',
+                    lastName: 'Burnside',
+                    image: 'https://randomuser.me/api/portraits/women/79.jpg',
+                    location: 'Minneapolis, MN',
+                    social: {
+                        twitter: 'https://twitter.com/e_neub',
+                        facebook: 'https://www.facebook.com/elleneub',
+                    },
+                    paymentMethods: ['PayPay', 'Cash'],
+                    skills: [
+                        'Errands',
+                        'Transportation',
+                        'Light housework',
+                        'Light yardwork',
+                        'Heavy housework',
+                        'Heavy yardwork',
+                    ],
+                },
+                isLoading: false,
+            })
+        }, 1000) // edit this to reduce spinner time
+    }
+
     render() {
+        const mainPage = this.state.isLoading ? (
+            <div className="d-flex justify-content-center spinner">
+                <div className="spinner-border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>
+        ) : (
+            <Switch>
+                <Route exact path="/offerHelp" component={OfferHelpPage} />
+                <Route exact path="/findHelp" component={FindHelpPage} />
+                <Route exact path="/messages" component={MessagesPage} />
+                <Route
+                    exact
+                    path="/account"
+                    render={() => {
+                        return <MyAccountPage userData={this.state.userData} />
+                    }}
+                />
+                <Route
+                    exact
+                    path="/guidelines"
+                    component={CommunityGuidelinesPage}
+                />
+                <Route exact path="/demo" component={DemoPage} />
+                <Route exact path="/home" component={HomePage} />
+                <Route exact path="" component={DemoPage} />
+            </Switch>
+        )
+
         return (
             <div>
                 <Navbar />
-                <Switch>
-                    <Route exact path="/offerHelp" component={OfferHelpPage} />
-                    <Route exact path="/findHelp" component={FindHelpPage} />
-                    <Route exact path="/messages" component={MessagesPage} />
-                    <Route exact path="/account" component={MyAccountPage} />
-                    <Route
-                        exact
-                        path="/guidelines"
-                        component={CommunityGuidelinesPage}
-                    />
-                    <Route exact path="/demo" component={DemoPage} />
-                    <Route exact path="/home" component={HomePage} />
-                    <Route exact path="" component={DemoPage} />
-                </Switch>
+                {mainPage}
             </div>
         )
     }
