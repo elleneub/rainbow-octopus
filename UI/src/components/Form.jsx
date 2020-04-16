89d6e11c78f65e5d40aa6a88f529f565344a6b7f
solgamd <solgamd@gmail.com>
2020-04-16 12:10:27 -0500

    FormPayments created

--- UI/src/components/Form.jsx
+++ UI/src/components/Form.jsx
@@ -1,42 +1,7 @@
 import React from 'react'
 import FormCategories from './FormCategories'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-    // faTwitter,
-    // faInstagram,
-    // faFacebook,
-    // faGithub,
-    faPaypal,
-    // faCcAmazonPay,
-    faCcApplePay,
-    // faCcPaypal,
-    faCcStripe,
-    // faCcMastercard,
-    // faCcAmex,
-    // faCcVisa,
-    faAmazonPay,
-    // faStripe,
-    // faStripeS,
-} from '@fortawesome/free-brands-svg-icons'
-import {
-    // faBars,
-    // faUser,
-    // faUsers,
-    // faUserAstronaut,
-    // faPeopleCarry,
-    // faUserSecret,
-    // faEdit,
-    // faUserEdit,
-    // faDonate,
-    // faHandHoldingHeart,
-    // faHandHoldingUsd,
-    // faMoneyBill,
-    faMoneyBillWave,
-    // faMoneyBillAlt,
-    // faMoneyBillWaveAlt,
-    // faCoins,
-    faHandshake,
-} from '@fortawesome/free-solid-svg-icons'
+import FormPayments from './FormPayments'
+
 import FormFrequency from './FormFrequency'
 
 class Form extends React.Component {
@@ -86,98 +51,7 @@ class Form extends React.Component {
 
                 <FormFrequency />
 
-                <div className="form-group">
-                    <label for="dropdown">
-                        What forms of payment can you accept?
-                    </label>
-                    <div className="preferred-payment">
-                        <div className="form-check form-check-inline">
-                            <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id="cash"
-                            />
-                            <label className="form-check-label" for="cash">
-                                <FontAwesomeIcon
-                                    icon={faMoneyBillWave}
-                                    className="mr-1"
-                                />
-                                Cash
-                            </label>
-                        </div>
-
-                        <div className="form-check form-check-inline">
-                            <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id="paypal"
-                            />
-                            <label className="form-check-label" for="paypal">
-                                <FontAwesomeIcon
-                                    icon={faPaypal}
-                                    className="mr-1"
-                                />
-                                PayPal
-                            </label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id="applepay"
-                            />
-                            <label className="form-check-label" for="applepay">
-                                <FontAwesomeIcon
-                                    icon={faCcApplePay}
-                                    className="mr-1"
-                                />
-                                Apple Pay
-                            </label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id="barter"
-                            />
-                            <label className="form-check-label" for="barter">
-                                <FontAwesomeIcon
-                                    icon={faHandshake}
-                                    className="mr-1"
-                                />
-                                Barter
-                            </label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id="stripe"
-                            />
-                            <label className="form-check-label" for="stripe">
-                                <FontAwesomeIcon
-                                    icon={faCcStripe}
-                                    className="mr-1"
-                                />
-                                Stripe
-                            </label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id="amazonpay"
-                            />
-                            <label className="form-check-label" for="amazonpay">
-                                <FontAwesomeIcon
-                                    icon={faAmazonPay}
-                                    className="mr-1"
-                                />
-                                AmazonPay
-                            </label>
-                        </div>
-                    </div>
-                </div>
+           <FormPayments />
 
                 <div className="button-form mt-4">
                     <button type="submit" className="btn btn-primary">
