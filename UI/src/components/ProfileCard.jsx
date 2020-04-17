ce9859edb57b3f1158c53d3036aff057ea05a75b
solgamd <solgamd@gmail.com>
2020-04-17 18:28:03 -0500

    Fixed typo 'PayPay' in MyAccount and ProfileCard

--- UI/src/components/ProfileCard.jsx
+++ UI/src/components/ProfileCard.jsx
@@ -11,7 +11,7 @@ import {
 
 class ProfileCard extends React.Component {
     paymentIcons = {
-        PayPay: faCcPaypal,
+        PayPal: faCcPaypal,
         Stripe: faCcStripe,
         Cash: faMoneyBillWave,
     }
@@ -74,39 +74,38 @@ class ProfileCard extends React.Component {
                             Send {data.first_name} a Message
                         </button>
                     </div>
-                    <div className="col-6">
-                        <h4>
-                            {data.first_name + ' ' + data.last_name}
-                        </h4>
-                        <h6 className="my-3">
-                            {data.addresses[0].city +
-                                ', ' +
-                                data.addresses[0].state}
-                        </h6>
-                        <div className="social-icons">{socialSection}</div>
-                        <div className="payment mb-3">
-                            <h6>
-                                I accept:
-                                <ul className="col justify-content-around mt-1">
-                                    {paymentSection}
-                                </ul>
-                            </h6>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <hr></hr>
-                        <div className="skills">
-                            <h5 className="text-center m-3">
-                                I Can Help With:
-                            </h5>
-                            <ul>
-                                {data.skills.map((skill) => {
-                                    return <li key={skill}>{skill}</li>
-                                })}
+                </div>
+
+                <div className="col-6 text-left">
+                    <h3>{data.first_name + ' ' + data.last_name}</h3>
+                    <h5 className="my-3">
+                        {data.addresses[0].city +
+                            ', ' +
+                            data.addresses[0].state}
+                    </h5>
+                    <div className="social-icons">{socialSection}</div>
+
+                    <div className="payment mb-3 text-left">
+                        <h5>
+                            I accept:
+                            <ul className="col justify-content-around mt-1">
+                                {paymentSection}
                             </ul>
-                        </div>
+                        </h5>
                     </div>
                 </div>
+
+                {/* <div className="col">
+                    <hr></hr>
+                    <div className="skills">
+                        <h5 className="text-center m-3">I Can Help With:</h5>
+                        <ul>
+                            {data.skills.map((skill) => {
+                                return <li key={skill}>{skill}</li>
+                            })}
+                        </ul>
+                    </div>
+                </div> */}
             </div>
         )
     }
