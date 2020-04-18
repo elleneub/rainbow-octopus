e3476b4f965539abee063d5d42c55ecee7723f85
solgamd <solgamd@gmail.com>
2020-04-17 21:10:06 -0500

    Added messages and search bar

--- ui/src/pages/Messages.jsx
+++ ui/src/pages/Messages.jsx
@@ -5,6 +5,52 @@ class MessagesPage extends React.Component {
         return (
             <div>
                 <h1 className="page-title">Messages Page</h1>
+
+                <div className="search-bar mx-5">
+                    <h6>Search</h6>
+                </div>
+                <div className="media py-2 my-2">
+                    <img
+                        src="..."
+                        className="align-self-center mr-3"
+                        alt="..."
+                    />
+                    <div className="media-body">
+                        <h5 className="mt-0">Neighbor</h5>
+                        <p className="mb-0">
+                            Aye list pirate parrel hardtack rigging barque belay
+                            barkadeer bucko. Doubloon shrouds weigh anchor...
+                        </p>
+                    </div>
+                </div>
+                <div className="media media-color py-2 my-2">
+                    <img
+                        src="..."
+                        className="align-self-center mr-3"
+                        alt="..."
+                    />
+                    <div className="media-body">
+                        <h5 className="mt-0">Neighbor</h5>
+                        <p className="mb-0">
+                            Aye list pirate parrel hardtack rigging barque belay
+                            barkadeer bucko. Doubloon shrouds weigh anchor...
+                        </p>
+                    </div>
+                </div>
+                <div className="media py-2 my-2">
+                    <img
+                        src="..."
+                        className="align-self-center mr-3"
+                        alt="..."
+                    />
+                    <div className="media-body">
+                        <h5 className="mt-0">Neighbor</h5>
+                        <p className="mb-0">
+                            Aye list pirate parrel hardtack rigging barque belay
+                            barkadeer bucko. Doubloon shrouds weigh anchor...
+                        </p>
+                    </div>
+                </div>
             </div>
         )
     }
